57518bb27d99a45c03aac74b0e98d6afb3479a21
mariaparfenyuk <umme0807@gmail.com>
2023-08-30 22:56:42 +0200

    add timer

--- public/index.js
+++ public/index.js
@@ -1,26 +1,42 @@
 const taskElement = document.getElementById("task");
 const timerElement = document.getElementById("timer");
 
-function updateTaskAndTimer() {
-  const tasks = [
-    "Spend 5 minutes in meditation.",
-    "Take a short walk outside.",
-    "Write down a list of things you're grateful for today.",
-    "Read a chapter from a book you enjoy.",
-    "Practice deep breathing for 10 minutes.",
-  ];
-  
-  const currentTask = tasks[Math.floor(Math.random() * tasks.length)];
-  taskElement.textContent = `Task: ${currentTask}`;
+const tasks = [
+  "Spend 5 minutes in meditation.",
+  "Take a short walk outside.",
+  "Write down a list of things you're grateful for today.",
+  "Read a chapter from a book you enjoy.",
+  "Practice deep breathing for 10 minutes.",
+];
 
-  const timeUntilMidnight = new Date();
-  timeUntilMidnight.setHours(24, 0, 0, 0);
-  const timeDifference = timeUntilMidnight - new Date();
+let currentTaskIndex = Math.floor(Math.random() * tasks.length);
+
+function updateTask() {
+  currentTaskIndex = (currentTaskIndex + 1) % tasks.length;
+  taskElement.textContent = `Task: ${tasks[currentTaskIndex]}`;
+}
+
+function getTimeUntilMidnightGMT() {
+  const now = new Date();
+  const utcNow = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0);
+  const midnightGMT = new Date(utcNow.getTime() + 24 * 60 * 60 * 1000);
+  
+  return midnightGMT.getTime() - now.getTime();
+}
+
+function updateTimer() {
+  const timeDifference = getTimeUntilMidnightGMT();
   const hours = Math.floor(timeDifference / (60 * 60 * 1000));
   const minutes = Math.floor((timeDifference % (60 * 60 * 1000)) / (60 * 1000));
   const seconds = Math.floor((timeDifference % (60 * 1000)) / 1000);
-  timerElement.textContent = `Time until next task: ${hours}h ${minutes}m ${seconds}s`;
+  
+  timerElement.textContent = `Time until midnight GMT: ${hours}h ${minutes}m ${seconds}s`;
+}
+
+function updateTaskAndTimer() {
+  updateTask();
+  updateTimer();
 }
 
 updateTaskAndTimer();
-setInterval(updateTaskAndTimer, 1000);
+setInterval(updateTimer, 1000);
